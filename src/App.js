7e9d9ff2a533349b58b5576b102db9027c65b8d1
Ysquid <ahk648@gmail.com>
2022-12-05 11:31:36 -0600

    basic routes added with links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BookList from "./pages/BookList";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    // Routes needs to wrap all individual Route
+    <>
+      {/* Link is like our anchor tags, but specifically for navigation */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        {/* path is like the url we are going to
+      so for instance'/' goes to our root (in this case index.js) */}
+        {/* element is the component we want to render at that route */}
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+      </Routes>
+    </>
   );
 }
 
